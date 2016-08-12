e5c26d556c27590b964abed68f5fda064ccfa42c
keeva <mm9mm8@126.com>
2016-08-12 18:18:08 +0800

    update

--- ch1/e1-13.c
+++ ch1/e1-13.c
@@ -1,44 +1,73 @@
 //Write a program to print a histogram of the lengths of words in its input. 
 //It is easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging.
 
+/* assume word only contains letters. "I'm" is treated as two words "I" and "m" */
+
+
 //horizontal histogram
 
-/* assume there is no ' like in " I'm "  in the text */
+/*
 #include <stdio.h>
 
 #define MAXLENGTH 10
 
 int isletter(char c);
+int intlong(int a);
+int power(int x, int y);
+
 int main() {
 	int c;
 	int wordlen;
 	int inword = 0;
 	int letter;
-	long wordlenarr[MAXLENGTH +1];
-	long maxq = 0;
+	int end = 0;
+	int wordlenarr[MAXLENGTH +1];
+	int maxq = 0;
 	int index = 0;
 	int i;
 
 	for (; index <= MAXLENGTH ; index++) 
 		wordlenarr[index] = 0;
 
-	while ((c = getchar()) != EOF) {
-		letter = isletter(c);
-		if (!inword) {
-			if (letter) {
-				wordlen = 1;
-				inword = 1;
+	while (!end) {
+		c = getchar();
+		if (c != EOF) {
+			letter = isletter(c);
+			if (!inword) {
+				if (letter) {
+					wordlen = 1;
+					inword = 1;
+				}
+			} else {
+				if (letter) {
+					wordlen++;
+				} else {
+					if(wordlen <= MAXLENGTH) {
+						wordlen = wordlen - 1;
+					} else {
+						wordlen = MAXLENGTH;
+					}
+					wordlenarr[wordlen]++;
+					if(maxq < wordlenarr[wordlen]) {
+						maxq = wordlenarr[wordlen];
+					}
+					inword = 0;
+				}	
 			}
 		} else {
-			if (letter) {
-				wordlen++;
-			} else {
-				wordlen <= MAXLENGTH ? wordlenarr[wordlen-1]++ : wordlenarr[MAXLENGTH]++ ;
-				if(maxq < wordlenarr[wordlen-1]) {
-					maxq = wordlenarr[wordlen-1];
+			end = 1;
+			if(inword && wordlen > 0) {
+				if(wordlen <= MAXLENGTH) {
+					wordlen = wordlen - 1;
+				} else {
+					wordlen = MAXLENGTH;
+				}
+				wordlenarr[wordlen]++;
+				if(maxq < wordlenarr[wordlen]) {
+					maxq = wordlenarr[wordlen];
 				}
 				inword = 0;
-			}	
+			}
 		}
 		
 	}
@@ -52,21 +81,157 @@ int main() {
 		for (i =0; i< wordlenarr[index]; i++) {
 			printf("█");
 		}
-		printf("\t %ld \n", wordlenarr[index]);
+		printf("   %d \n", wordlenarr[index]);
 	}
 	printf("    |");
-	//printf("%ld\n", maxq);
 	for (index = 0; index <= maxq; index++ ) {
 		printf("_");
 	}
-	printf("\n    0");
-	for (index=0; index < maxq; index++ ) {
-		printf(" ");
+
+	//  print horizontal ordinate dimension 
+	printf("\n     ");
+	int forprint[maxq];
+	for (index = 0; index < maxq; ++index) {
+		forprint[index] = index+1;
 	}
-	printf("%ld\n\n",maxq);
+	for (i= intlong(maxq); i > 0; i-- ) {
+		int base = power(10, i-1);
+		for (index = 0; index < maxq; ++index)
+		{
+			if(forprint[index]/base > 0) {
+				printf("%d", forprint[index]/base);
+				forprint[index] = forprint[index]%base ;
+			} else {
+				printf("0");
+			}
+		}
+		printf("\n     ");
+	}
+
+
 	return 0;
 }
 
 int isletter(char c) {
 	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ? c : 0 ;
 }
+int intlong(int a) {
+	int i = 1;
+	int p = a/10;
+	if(p > 0) {
+		i = i + intlong(p);
+	}
+	return i;
+}
+
+int power(int x, int y) {
+	int r = 1;
+	for (int i = 0; i < y; ++i)
+	{
+		r = r * x ;
+	}
+	return r;
+}
+
+*/
+
+
+
+//vertical histogram
+
+#include <stdio.h>
+
+#define MAXLENGTH 10
+
+int isletter(char c);
+
+int main() {
+	int c;
+	int wordlen;
+	int inword = 0;
+	int letter;
+	int end = 0;
+	int wordlenarr[MAXLENGTH +1];
+	int maxq = 0;
+	int index = 0;
+	int i;
+
+	for (; index <= MAXLENGTH ; index++) 
+		wordlenarr[index] = 0;
+
+	while (!end) {
+		c = getchar();
+		if (c != EOF) {
+			letter = isletter(c);
+			if (!inword) {
+				if (letter) {
+					wordlen = 1;
+					inword = 1;
+				}
+			} else {
+				if (letter) {
+					wordlen++;
+				} else {
+					if(wordlen <= MAXLENGTH) {
+						wordlen = wordlen - 1;
+					} else {
+						wordlen = MAXLENGTH;
+					}
+					wordlenarr[wordlen]++;
+					if(maxq < wordlenarr[wordlen]) {
+						maxq = wordlenarr[wordlen];
+					}
+					inword = 0;
+				}	
+			}
+		} else {
+			end = 1;
+			if(inword && wordlen > 0) {
+				if(wordlen <= MAXLENGTH) {
+					wordlen = wordlen - 1;
+				} else {
+					wordlen = MAXLENGTH;
+				}
+				wordlenarr[wordlen]++;
+				if(maxq < wordlenarr[wordlen]) {
+					maxq = wordlenarr[wordlen];
+				}
+				inword = 0;
+			}
+		}
+	}
+	printf("\n");
+	for(index = 0; index < maxq; index++) {
+		printf("\n%6d |", maxq - index);
+		for(i = 0; i<= MAXLENGTH ; i++) {
+			if(wordlenarr[i] >= maxq - index) {
+				printf(" █ ");
+			} else {
+				printf("   ");
+			}
+		}
+	}
+	printf("\n        ");
+	for(i = 0; i<= MAXLENGTH ; i++) {
+		printf("---");
+	}
+	printf("\n       0");
+	for(i = 0; i<= MAXLENGTH ; i++) {
+		if(i == MAXLENGTH) {
+			printf(">10");
+		} else {
+			printf("%2d ",i+1);
+		}
+	}
+	printf("\n\n");
+	return 0;
+}
+
+int isletter(char c) {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ? c : 0 ;
+}
+
+
+
+
+
---
+++ ch1/e1-14.c
@@ -0,0 +1,8 @@
+//Write a program to print a histogram of the frequencies of different characters in its input.
+
+
+
+
+
+
+
